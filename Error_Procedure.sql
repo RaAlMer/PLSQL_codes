889c7e61004a0894599e3cc79b5e292bb0c268a2
Raúl Alonso <67586098+SpaSniper@users.noreply.github.com>
2021-08-31 22:36:45 +0200

    Create Error_Procedure.sql

---
+++ Error_Procedure.sql
@@ -0,0 +1,64 @@
+-- Tabla de errores
+CREATE TABLE error_log
+(
+  id              NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title           VARCHAR2 (200 CHAR),
+  info            CLOB,
+  created_on      DATE DEFAULT SYSDATE,
+  created_by      VARCHAR2 (100 CHAR),
+  callstack       CLOB
+  errorstack      CLOB,
+  errorbacktrace  CLOB
+ )
+ /
+ 
+ -- Procedimiento para manejar el error
+ CREATE OR REPLACE PROCEDURE log_error (title_in    IN error_log.title%TYPE,
+                                        info_in     IN error_log.info%TYPE)
+  
+  AUTHID DEFINER
+IS
+  PRAGMA AUTONOMOUS_TRANSACTION;
+BEGIN
+  INSERT INTO error_log (title,
+                         info,
+                         created_by,
+                         callstack,
+                         errorstack,
+                         errorbacktrace)
+  VALUES (title_in,
+          info_in,
+          USER,
+          DBMS_UTILITY.format_call_stack,
+          DBMS_UTILITY.format_error_stack,
+          DBMS_UTILITY.format_error_backtrace);
+ COMMIT;
+END;
+/
+
+-- Mi procedimiento para probar
+CREATE OR REPLACE PROCEDURE my_procedure (value_in IN INTEGER)
+  AUTHID DEFINER
+IS
+  l_local_variable  DATE;
+BEGIN
+  l_local_variable :=
+    CASE WHEN value_in > 100 THEN SYSDATE - 10 ELSE SYSDATE + 10 END;
+    
+    UPDATE employees
+      SET salary = value_in;
+      
+    RAISE PROGRAM_ERROR;
+EXCEPTION
+  WHEN OTHERS
+  THEN
+    log_error (
+      'my_procedure failed',
+        'value_in = '
+       || value_in
+       || 'l_local_variable = '
+       || TO_CHAR (l_local_variable, 'DD/MM/YYYY HH24:MI:SS'));
+       
+    RAISE;
+END;
+/
