28ba82e4e306455cb317f1b044ad6dbb85552c8f
Raúl Alonso <67586098+SpaSniper@users.noreply.github.com>
2021-08-31 23:43:00 +0200

    Update Error_Procedure.sql

--- Error_Procedure.sql
+++ Error_Procedure.sql
@@ -1,33 +1,33 @@
 -- Tabla de errores
-CREATE TABLE error_log
+CREATE TABLE tb_error_log
 (
-  id              NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  title           VARCHAR2 (200 CHAR),
-  info            CLOB,
-  created_on      DATE DEFAULT SYSDATE,
-  created_by      VARCHAR2 (100 CHAR),
-  callstack       CLOB,
-  errorstack      CLOB,
-  errorbacktrace  CLOB
+  vn_id              NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  vv_title           VARCHAR2 (200 CHAR),
+  vc_info            CLOB,
+  vd_created_on      DATE DEFAULT SYSDATE,
+  vv_created_by      VARCHAR2 (100 CHAR),
+  vc_callstack       CLOB,
+  vc_errorstack      CLOB,
+  vc_errorbacktrace  CLOB
  )
  /
  
  -- Procedimiento para manejar el error
- CREATE OR REPLACE PROCEDURE pr_log_error (title_in    IN error_log.title%TYPE,
-                                           info_in     IN error_log.info%TYPE)
+ CREATE OR REPLACE PROCEDURE pr_log_error (pi_title    IN tb_error_log.title%TYPE,
+                                           pi_info     IN tb_error_log.info%TYPE)
   
   AUTHID DEFINER
 IS
   PRAGMA AUTONOMOUS_TRANSACTION;
 BEGIN
-  INSERT INTO error_log (title,
-                         info,
-                         created_by,
-                         callstack,
-                         errorstack,
-                         errorbacktrace)
-  VALUES (title_in,
-          info_in,
+  INSERT INTO tb_error_log (vv_title,
+                            vc_info,
+                            vd_created_by,
+                            vc_callstack,
+                            vc_errorstack,
+                            vc_errorbacktrace)
+  VALUES (pi_title,
+          pi_info,
           USER,
           DBMS_UTILITY.format_call_stack,
           DBMS_UTILITY.format_error_stack,
@@ -37,16 +37,16 @@ END;
 /
 
 -- Mi procedimiento para probar
-CREATE OR REPLACE PROCEDURE pr_my_procedure (value_in IN INTEGER)
+CREATE OR REPLACE PROCEDURE pr_my_procedure (pi_value IN INTEGER)
   AUTHID DEFINER
 IS
-  l_local_variable  DATE;
+  d_local_variable  DATE;
 BEGIN
-  l_local_variable :=
-    CASE WHEN value_in > 100 THEN SYSDATE - 10 ELSE SYSDATE + 10 END;
+  d_local_variable :=
+    CASE WHEN pi_value > 100 THEN SYSDATE - 10 ELSE SYSDATE + 10 END;
     
     UPDATE employees
-      SET salary = value_in;
+      SET salary = pi_value;
       
     RAISE PROGRAM_ERROR;
 EXCEPTION
@@ -54,10 +54,10 @@ EXCEPTION
   THEN
     pr_log_error (
       'my_procedure failed',
-        'value_in = '
-       || value_in
-       || 'l_local_variable = '
-       || TO_CHAR (l_local_variable, 'DD/MM/YYYY HH24:MI:SS'));
+        'pi_value = '
+       || pi_value
+       || 'd_local_variable = '
+       || TO_CHAR (d_local_variable, 'DD/MM/YYYY HH24:MI:SS'));
        
     RAISE;
 END;
