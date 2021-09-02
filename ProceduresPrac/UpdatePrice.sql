6407e2a41c5a1865756ca14f75c4b32604308d7a
Raúl Alonso <67586098+SpaSniper@users.noreply.github.com>
2021-09-02 14:14:44 +0200

    Update UpdatePrice.sql

--- ProceduresPrac/UpdatePrice.sql
+++ ProceduresPrac/UpdatePrice.sql
@@ -45,43 +45,43 @@ END;
 /
 
 -- Procedimiento para actualizar los precios
-CREATE OR REPLACE PROCEDURE pr_actualizar_precios_proveedor (pi_n_idproducto           IN         TB_PRODUCTOS.N_IDPRODUCTO%TYPE,
-                                                             pio_n_precioud            IN OUT     TB_PRODUCTOS.N_PRECIOUD%TYPE,
-                                                             po_n_porcentajecambio     OUT        TB_PRODUCTOS.N_PORCENTAJE%TYPE)
+CREATE OR REPLACE PROCEDURE pr_actualizar_precios_proveedor (pi_n_idproducto           IN         TB_PRODUCTOS.N_IDPRODUCTO%TYPE, --Se introduce el ID del producto
+                                                             pio_n_precioud            IN OUT     TB_PRODUCTOS.N_PRECIOUD%TYPE,   --Se introduce el nuevo precio
+                                                             po_n_porcentajecambio     OUT        TB_PRODUCTOS.N_PORCENTAJE%TYPE) --Sale el porcentaje (nuevo precio/antiguo precio)
 IS  
-    vn_idproducto   TB_PRODUCTOS.N_IDPRODUCTO%TYPE;
-    vn_porcentaje   TB_PRODUCTOS.N_PORCENTAJE%TYPE;
-    vn_precioud     TB_PRODUCTOS.N_PRECIOUD%TYPE;
-    ex_sinid        EXCEPTION;
-    ex_sinprecio    EXCEPTION;
+    vn_idproducto   TB_PRODUCTOS.N_IDPRODUCTO%TYPE; --Variable para guardar el ID del producto
+    vn_precioud     TB_PRODUCTOS.N_PRECIOUD%TYPE;   --Variable para guardar el antiguo precio
+    vn_porcentaje   TB_PRODUCTOS.N_PORCENTAJE%TYPE; --Variable para guardar el porcentaje
+    ex_sinid        EXCEPTION; --Excepción si no se introduce ID del producto
+    ex_sinprecio    EXCEPTION; --Excepción si no se introduce el precio nuevo que actualiza el antiguo
     
 BEGIN
-    vn_idproducto  := NVL(pi_n_idproducto, -1);
-    pio_n_precioud := NVL(pio_n_precioud, -1);
+    vn_idproducto  := NVL(pi_n_idproducto, -1); --Si no se introduce ID se pone -1
+    pio_n_precioud := NVL(pio_n_precioud, -1); --Si no se introduce precio nuevo se pone -1
     
     IF pio_n_precioud = -1 THEN
-        RAISE ex_sinprecio;
+        RAISE ex_sinprecio; --Se lanza el error si no hay ID (ID = -1)
     ELSIF vn_idproducto = -1 THEN
-        RAISE ex_sinid;
+        RAISE ex_sinid; --Se lanza el error si no hay precio nuevo (precio = -1)
     END IF;
     
     BEGIN
-        SELECT n_precioud
+        SELECT n_precioud --Consulta para introducir el precio antiguo en su variable (vn_precioud)
          INTO vn_precioud
          FROM tb_productos
          WHERE n_idproducto = vn_idproducto;
     END;
     BEGIN
-        vn_porcentaje := (pio_n_precioud/vn_precioud)*100;
+        vn_porcentaje := (pio_n_precioud/vn_precioud)*100; --Cálculo del porcentaje entre precios nuevo y antiguo
     END;
     
     BEGIN
-        UPDATE tb_productos t
+        UPDATE tb_productos t --Consulta de actualización del precio nuevo donde estaba el antiguo
          SET t.n_precioud = 
                         (SELECT
                         CASE
-                            WHEN st.b_activo = 'FALSE' THEN st.n_precioud
-                            WHEN st.b_activo = 'TRUE'  THEN pio_n_precioud
+                            WHEN st.b_activo = 'FALSE' THEN st.n_precioud --Si el producto está vencido, no sustituye el precio nuevo
+                            WHEN st.b_activo = 'TRUE'  THEN pio_n_precioud --Si el producto está activo, si sustituye el precio nuevo
                             END
                         FROM tb_productos st
                         WHERE st.n_idproducto = vn_idproducto)
@@ -89,24 +89,24 @@ BEGIN
     END;
     
     BEGIN
-        UPDATE tb_productos
+        UPDATE tb_productos --Consulta de actualización del porcentaje en su fila correspondiente
             SET n_porcentaje = vn_porcentaje
             WHERE n_idproducto = vn_idproducto;
     END;
     
 EXCEPTION
     WHEN ex_sinprecio THEN
-        DBMS_OUTPUT.PUT_LINE('No se introdujo nuevo precio.');
+        DBMS_OUTPUT.PUT_LINE('No se introdujo nuevo precio.'); --Error si no hay precio nuevo
     WHEN ex_sinid THEN
-        DBMS_OUTPUT.PUT_LINE('No se introdujo ID.');
+        DBMS_OUTPUT.PUT_LINE('No se introdujo ID.'); --Error si no se introdujo ID
         
 END pr_actualizar_precios_proveedor;
 /
 -- Llamamos al procedimiento
 DECLARE
-    vn_id           NUMBER := 1;
-    vn_porcentaje   NUMBER;
-    vn_precioud     NUMBER := 4.5;
+    vn_id           NUMBER := 1; --Introducimos el ID del producto del que queremos sustituir el precio
+    vn_porcentaje   NUMBER; --Variable de salida del porcentaje
+    vn_precioud     NUMBER := 4.5; --Introducimos el nuevo precio
 BEGIN
     pr_actualizar_precios_proveedor(vn_id, vn_precioud, vn_porcentaje);
 END;
