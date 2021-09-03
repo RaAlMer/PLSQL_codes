f587970c59c2d1b9c0ea8acb5c096a7d6d90cace
Raúl Alonso <67586098+SpaSniper@users.noreply.github.com>
2021-09-03 13:41:31 +0200

    Update SellUnitsProd.sql

--- ProceduresPrac/SellUnitsProd.sql
+++ ProceduresPrac/SellUnitsProd.sql
@@ -6,6 +6,7 @@ IS
     vn_unidades     TB_PRODUCTOS.N_UNIDADES%TYPE; --Variable para almacenar el número de unidades antiguas
     ex_sinid        EXCEPTION; --Excepción si no se introduce ID del producto
     ex_sinud        EXCEPTION; --Excepción si no se introducen las unidades del producto
+    ex_udneg        EXCEPTION; --Excepción si se venden más unidades de producto de las que hay en el almacén
     
 BEGIN
     vn_idproducto  := NVL(pi_n_idproducto, -1); --Si no se introduce ID se pone -1
@@ -24,6 +25,10 @@ BEGIN
          WHERE n_idproducto = vn_idproducto;
     END;
     
+    IF pio_n_unidades > vn_unidades THEN
+        RAISE ex_udneg; --Lanza el error si se venden más unidades de las que hay
+    END IF;
+    
     BEGIN
         vn_unidades := vn_unidades - pio_n_unidades; --Actualizamos el valor de las unidades restando las introducidas como parámetro
     END;
@@ -39,6 +44,8 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('No se introdujeron unidades.'); --Error si no hay unidades del producto
     WHEN ex_sinid THEN
         DBMS_OUTPUT.PUT_LINE('No se introdujo ID.'); --Error si no se introdujo ID
+    WHEN ex_udneg THEN
+        DBMS_OUTPUT.PUT_LINE('¡No hay tantas unidades para vender!'); --Error si se venden más unidades de las que hay disponibles
         
 END pr_vender_productos;
 
