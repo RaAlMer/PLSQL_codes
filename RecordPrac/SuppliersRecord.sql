ee0b3af962f615cd5028b1c02ae83b048cdb4a3c
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2021-10-01 22:51:45 +0200

    Create SuppliersRecord.sql

---
+++ RecordPrac/SuppliersRecord.sql
@@ -0,0 +1,29 @@
+DECLARE
+	TYPE t_registro IS RECORD(
+		v_producto  VARCHAR2(20),
+		n_unidades  NUMBER(3),
+		d_fechaalta DATE);
+
+	vtr_producto t_registro;
+
+	vr_proveedores tbproveedores%ROWTYPE;
+
+BEGIN
+	SELECT *
+	 INTO vr_proveedores
+	 FROM tbproveedores
+	WHERE n_idproveedor = 1;
+
+	DBMS_OUTPUT.PUT_LINE('El ID del proveedor es ' || vr_proveedores.n_idproveedor);
+	DBMS_OUTPUT.PUT_LINE('El nombre del proveedor es ' || vr_proveedores.v_desproveedor);
+	DBMS_OUTPUT.PUT_LINE('La fecha de alta es ' || TO_CHAR(vr_proveedores.d_fechaalta, 'DD/MM/YYYY'));	
+
+	vtr_producto.v_producto  := 'Martillo';
+	vtr_producto.n_unidades  := 10;
+	vtr_producto.d_fechaalta := SYSDATE;
+
+	DBMS_OUTPUT.PUT_LINE('El producto es ' || vtr_producto.v_producto);
+	DBMS_OUTPUT.PUT_LINE('Unidades: ' || vtr_producto.n_unidades);
+	DBMS_OUTPUT.PUT_LINE('La fecha de alta es ' || TO_CHAR(vtr_producto.d_fechaalta, 'DD/MM/YYYY'));
+
+END;
