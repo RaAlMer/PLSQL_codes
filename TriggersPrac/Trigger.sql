a89ab798460bb0babbad15c3cc542cc1bcdca6cc
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2021-10-01 10:50:05 +0200

    Update Trigger.sql

--- TriggersPrac/Trigger.sql
+++ TriggersPrac/Trigger.sql
@@ -1,18 +1,4 @@
---Supplier's table
-CREATE TABLE tbproveedores
-(
-    n_idproveedor   NUMBER(3),
-    v_desproveedor  VARCHAR2(50 CHAR),
-    d_fechaalta     DATE
-);
-
-INSERT INTO tbproveedores
-    VALUES (1, 'Martillos La Forja', TO_DATE('19/04/2021', 'DD/MM/YYYY'));
-INSERT INTO tbproveedores 
-    VALUES (2, 'Materiales El Bueno', TO_DATE('01/05/2021', 'DD/MM/YYYY'));
-INSERT INTO tbproveedores 
-    VALUES (3, 'Almacenes Los Mejores', TO_DATE('07/05/2021', 'DD/MM/YYYY'));
-
+-- Log table to track any changes in suppliers table
 CREATE TABLE tb_logproveedores
 (
     n_idproveedorold  NUMBER(3),
@@ -25,19 +11,20 @@ CREATE TABLE tb_logproveedores
     v_accion          VARCHAR2(10 CHAR)
 );
 
+-- Trigger that gets any change in suppliers' table to the log table
 CREATE OR REPLACE TRIGGER tr_log_proveedores
 AFTER INSERT OR UPDATE OR DELETE ON tbproveedores
 FOR EACH ROW
 DECLARE
-    vv_accion VARCHAR2(10 CHAR);
+    vv_accion VARCHAR2(10 CHAR); --Variable to save the action taken in the suppliers' table
 BEGIN
     CASE
-        WHEN INSERTING THEN vv_accion := 'INSERT';
-        WHEN UPDATING THEN vv_accion := 'UPDATE';
-        WHEN DELETING THEN vv_accion := 'DELETE';
+        WHEN INSERTING THEN vv_accion := 'INSERT'; --Action: insert
+        WHEN UPDATING THEN vv_accion := 'UPDATE';  --Action: update
+        WHEN DELETING THEN vv_accion := 'DELETE';  --Action: delete
     END CASE;
     
-    INSERT INTO tb_logproveedores (n_idproveedorold, n_idproveedornew,
+    INSERT INTO tb_logproveedores (n_idproveedorold, n_idproveedornew, -- Insert the old and new records after a change is made in the suppliers' table
                                    v_desproveedorold, v_desproveedornew,
                                    d_fechaaltaold, d_fechaaltanew,
                                    d_fecharegistro, v_accion)
@@ -48,12 +35,12 @@ BEGIN
  
 END tr_log_proveedores;
 
-INSERT INTO tbproveedores (n_idproveedor, v_desproveedor, d_fechaalta)
+INSERT INTO tbproveedores (n_idproveedor, v_desproveedor, d_fechaalta) --Insert test
 VALUES (4, 'Ferreteria Los Franceses', SYSDATE);
 
-UPDATE tbproveedores
+UPDATE tbproveedores --Update test
 SET d_fechaalta = SYSDATE
 WHERE n_idproveedor = 2;
 
-DELETE FROM tbproveedores
+DELETE FROM tbproveedores --Delete test
 WHERE n_idproveedor = 4;
