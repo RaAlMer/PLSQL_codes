0772334cb8c8affed0a457441f81def9c93cc8b7
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2021-10-02 14:45:49 +0200

    Update Collection.sql

--- CollectionsPrac/Collection.sql
+++ CollectionsPrac/Collection.sql
@@ -1,3 +1,4 @@
+-- VARRAY
 DECLARE
     -- Creamos el tipo
     TYPE ttab_ty_nombre IS VARRAY(5) OF VARCHAR2(20 CHAR);
@@ -42,5 +43,28 @@ BEGIN
     END IF;
     
     DBMS_OUTPUT.PUT_LINE('El número de registros en el vector es: ' || vttab_ty_nombre.COUNT);
+    
+END;
+
+-- BLOQUES ANIDADOS
+DECLARE
+    -- Creamos el tipo
+    TYPE ttab_ty_nombre IS TABLE OF VARCHAR2(20 CHAR);
+    -- Declaramos e inicializamos una variable del tipo creado
+    vttab_ty_nombre ttab_ty_nombre := ttab_ty_nombre(NULL, NULL, NULL);
+    
+BEGIN
+    
+    DBMS_OUTPUT.PUT_LINE('El número de registros en el vector es: ' || vttab_ty_nombre.COUNT);
+    
+    FOR i IN vttab_ty_nombre.FIRST .. vttab_ty_nombre.LAST LOOP
+        vttab_ty_nombre(i) := 'Prueba' || TO_CHAR(i);
+    END LOOP;
+    
+    IF vttab_ty_nombre.COUNT > 0 THEN
+        FOR i IN vttab_ty_nombre.FIRST .. vttab_ty_nombre.LAST LOOP
+            DBMS_OUTPUT.PUT_LINE('El ID del registro es: ' || vttab_ty_nombre(i));
+        END LOOP;
+    END IF;
     
 END;
