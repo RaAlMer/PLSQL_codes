dcc93d00a298ae192c771998c02d4ee2d376f5e3
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2021-10-02 14:39:17 +0200

    Update Collection.sql

--- CollectionsPrac/Collection.sql
+++ CollectionsPrac/Collection.sql
@@ -23,3 +23,24 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('El número de registros en el vector es: ' || vttab_ty_nombre.COUNT);
     
 END;
+
+DECLARE
+    -- Creamos el tipo
+    TYPE ttab_ty_nombre IS VARRAY(5) OF VARCHAR2(20 CHAR);
+    -- Declaramos e inicializamos una variable del tipo creado
+    vttab_ty_nombre ttab_ty_nombre := ttab_ty_nombre(5, 2, 4, 6, 7);
+    
+BEGIN
+    
+    DBMS_OUTPUT.PUT_LINE('El número máximo de registros en el vector es: ' || vttab_ty_nombre.LIMIT);
+    DBMS_OUTPUT.PUT_LINE('El número de registros en el vector es: ' || vttab_ty_nombre.COUNT);
+    
+    IF vttab_ty_nombre.COUNT > 0 THEN
+        FOR i IN vttab_ty_nombre.FIRST .. vttab_ty_nombre.LAST LOOP
+            DBMS_OUTPUT.PUT_LINE('El ID del registro es: ' || vttab_ty_nombre(i));
+        END LOOP;
+    END IF;
+    
+    DBMS_OUTPUT.PUT_LINE('El número de registros en el vector es: ' || vttab_ty_nombre.COUNT);
+    
+END;
